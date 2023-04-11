5b4de55534457a9547b26fbb8c72fb3d9c64076f
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2023-04-10 23:05:44 -0400

    Added comments, cleaned up test

--- tst/count_onesTB.vhd
+++ tst/count_onesTB.vhd
@@ -5,93 +5,88 @@
 --  Architecture:  Behavioral
 --  Author:        Barak Binyamin
 --  Created:       12/24/19
---  Modified:
+--  Modified:      04/10/23
 --  VHDL'93
 -------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
 
-
+-- Name the component we are making
 entity count_onesTB is
 end count_onesTB;
 
+-- Note that there is no input/output declaration here
+
+-- Name the method of we are using to define our component
 architecture Behavioral of count_onesTB is
 
---test1
-type test_vector1 is record
-		Xin	: std_logic_vector((2)-1 downto 0);
-		count	: std_logic_vector((2)-1 downto 0);
-     end record;
-     
-type test_array is array (natural range <>) of test_vector1;
-constant test_vector_array1 : test_array := (
-	(Xin => "00",count => "00"),
-	(Xin => "10",count => "01"),
-	(Xin => "11",count => "10")
-	);
-	
---test2
-type test_vector2 is record
-		Xin	: std_logic_vector((4)-1 downto 0);
-		count	: std_logic_vector((4)-1 downto 0);
-     end record;
-     
---type test_array2 is array (natural range <>) of test_vector2;
---constant test_vector_array2 : test_array := (
---	(Xin => "0000",count => "0000"),
---	(Xin => "1000",count => "0001"),
---	(Xin => "1111",count => "0100")
---	);
-	
-----test2
---type test_vector3 is record
---		Xin	: std_logic_vector((32)-1 downto 0);
---		count	: std_logic_vector((32)-1 downto 0);
---     end record;
-     
---type test_array3 is array (natural range <>) of test_vector3;
---constant test_vector_array3 : test_array := (
---	(Xin => x"00000000",count => x"00000000"),
---	(Xin => x"10000000",count => x"00000001"),
---	(Xin => x"11111111",count => x"00001000")
---	);
+    -- Test 1 Answer Sheet
+    type test_vector1 is record
+            Xin	    : std_logic_vector((2)-1 downto 0);
+            count	: std_logic_vector((2)-1 downto 0);
+    end record;
 
-	SIGNAL Xone : std_logic_vector (2-1 DOWNTO 0);  
-	SIGNAL Xtwo : std_logic_vector (4-1 DOWNTO 0);   
-    SIGNAL Xthree : std_logic_vector (32-1 DOWNTO 0);  
-    SIGNAL COUNTone : std_logic_vector (2-1 DOWNTO 0);  
-	SIGNAL COUNTtwo : std_logic_vector (4-1 DOWNTO 0);   
+    type test_array is array (natural range <>) of test_vector1;
+    constant test_vector_array1 : test_array := (
+        (Xin => "00",count => "00"), -- 0 should have no  ones
+        (Xin => "10",count => "01"), -- 2 should have one ones
+        (Xin => "11",count => "10")  -- 3 should have two ones
+    );
+	
+    -- Test 2 Answer Sheet
+    type test_vector2 is record
+            Xin	   : std_logic_vector((4)-1 downto 0);
+            count  : std_logic_vector((3)-1 downto 0);
+    end record;
+
+    type test_array2 is array (natural range <>) of test_vector2;
+    constant test_vector_array2 : test_array2 := (
+        (Xin => "0000",count => "000"), -- 0  should have no    ones
+        (Xin => "1010",count => "010"), -- 10 should have one   ones
+        (Xin => "1101",count => "011")  -- 13 should have three ones
+    );
+     
+     
+    -- Decalre internal signals used to drive the components we are testing
+	SIGNAL Xone       : std_logic_vector (2-1  DOWNTO 0);  
+	SIGNAL Xtwo       : std_logic_vector (4-1  DOWNTO 0);   
+    SIGNAL Xthree     : std_logic_vector (32-1 DOWNTO 0);  
+    SIGNAL COUNTone   : std_logic_vector (2-1  DOWNTO 0);  
+	SIGNAL COUNTtwo   : std_logic_vector (4-1  DOWNTO 0);   
     SIGNAL COUNTthree : std_logic_vector (32-1 DOWNTO 0); 
-
-    signal clk	: std_logic := '0';
+	-- Constants used in our test logic
+    signal   clk	  : std_logic := '0';
 	CONSTANT clkDelay : TIME := 10 ns;
+    
 begin
 
+	-- Unit under test (UUT), This is where we instantiate components to test and connect them to our test signals
     UUT1: entity work.count_ones
 	GENERIC map( LOG_BIT_WIDTH => 1)
     Port map(
-        CLK => clk,
-        X => Xone,
+        CLK   => clk,
+        X     => Xone,
         count => COUNTone
     );
     
     UUT2: entity work.count_ones
 	GENERIC map( LOG_BIT_WIDTH => 2)
     Port map(
-        CLK => clk,
-        X => Xtwo,
+        CLK   => clk,
+        X     => Xtwo,
         count => COUNTtwo
     );
     
     UUT3: entity work.count_ones
 	GENERIC map( LOG_BIT_WIDTH => 5)
     Port map(
-        CLK => clk,
-        X => Xthree,
+        CLK   => clk,
+        X     => Xthree,
         count => COUNTthree
     );
 		
+-- This is a process, a way to program sequentially, best to leave this method of hardware declarion for testing and timing, rather than implementation
 clk_proc:process
 begin
 	clk <= '0';
@@ -102,69 +97,25 @@ end process;
 
 stim_proc:process
 begin
-    --test1
+    -- Test 1: Check if the output matches the expected, if not raise an alarm
     for i in 0 to test_vector_array1'length-1 loop
-		wait until clk='0';
+		wait until clk='0';                         -- wait until falling edge
 		Xone   <= test_vector_array1(i).Xin;
-        wait for 10ns;
+        wait for clkDelay;                          -- wait one clock cycle
         assert COUNTone = test_vector_array1(i).count
         report "COUNTone is not correct in test " & integer'image(i) severity error;
 	end loop;
-	wait until clk='0';
-	
-	 --test2
---    for i in 0 to test_vector_array2'length-1 loop
---		wait until clk='0';
---		Xtwo   <= test_vector_array2(i).Xin;
---        wait for 10ns;
---        assert COUNTtwo = test_vector_array2(i).count
---        report "COUNTtwo is not correct in test " & integer'image(i) severity error;
---	end loop;
---	wait until clk='0';
-		wait until clk='0';
-		Xtwo   <= "0000";
-        wait for 10ns;
-        assert COUNTtwo = "0000"
-        report "COUNTtwo is not correct in test " & integer'image(0) severity error;
-        wait until clk='0';
-		Xtwo   <= "1000";
-        wait for 10ns;
-        assert COUNTtwo = "0001"
-        report "COUNTtwo is not correct in test " & integer'image(1) severity error;
-        wait until clk='0';
-		Xtwo   <= "1111";
-        wait for 10ns;
-        assert COUNTtwo = "0100"
-        report "COUNTtwo is not correct in test " & integer'image(2) severity error;
-	wait until clk='0';
-	
-	 --test3
---    for i in 0 to test_vector_array3'length-1 loop
---		wait until clk='0';
---		Xthree   <= test_vector_array3(i).Xin;
---        wait for 10ns;
---        assert COUNTthree = test_vector_array3(i).count
---        report "COUNThree is not correct in test " & integer'image(i) severity error;
---	end loop;
---	wait until clk='0';
---(Xin => x"00000000",count => x"00000000"),
---	(Xin => x"10000000",count => x"00000001"),
---	(Xin => x"11111111",count => x"00001000")
-		wait until clk='0';
-		Xthree   <= x"00000000";
-        wait for 10ns;
-        assert COUNTthree = x"00000000"
-        report "COUNTtwo is not correct in test " & integer'image(0) severity error;
-        wait until clk='0';
-		Xthree   <= x"10000000";
-        wait for 10ns;
-        assert COUNTthree = x"00000001"
-        report "COUNTtwo is not correct in test " & integer'image(1) severity error;
-        wait until clk='0';
-		Xthree   <= x"11111111";
-        wait for 10ns;
-        assert COUNTthree = x"00001000"
-        report "COUNTtwo is not correct in test " & integer'image(2) severity error;
+    
+    -- Test 2: Note these tests are running in parallel
+    for i in 0 to test_vector_array2'length-1 loop
+		wait until clk='0';                         -- wait until falling edge
+		Xtwo  <= test_vector_array2(i).Xin;
+        wait for clkDelay;                          -- wait one clock cycle
+        assert COUNTtwo = test_vector_array2(i).count
+        report "COUNTtwo is not correct in test " & integer'image(i) severity error;
+	end loop;
+   
+    -- End of tests
 	wait until clk='0';
 
     assert false
